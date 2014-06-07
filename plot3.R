6ea42a8d9697209f6149b7041a14f0986eeb5f1f
surbhit7 <surbhitahuja@gmail.com>
2014-06-07 18:16:45 +0530

    edited plot 3 and plot 4

--- plot3.R
+++ plot3.R
@@ -13,7 +13,7 @@ datetime=strptime(datestring,"%Y-%m-%d %H:%M:%S") #Time object now
 data=cbind(data,datetime)
 
 png("plot3.png")
-with(data,{plot(Sub_metering_1,type="l",xlab="")
+with(data,{plot(datetime,Sub_metering_1,type="l",xlab="")
      points(Sub_metering_2,type="l",col="red")
      points(Sub_metering_3,type="l",col="blue")
      legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
--- plot4.R
+++ plot4.R
@@ -17,7 +17,7 @@ par("mfrow"=c(2,2))
 plota= with(data,plot(datetime,Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="",main=""))
 
 plotb = with(data,plot(datetime,Voltage ,type="l",main=""))
-plotc= with(data,{plot(Sub_metering_1,type="l",xlab="")
+plotc= with(data,{plot(datetime,Sub_metering_1,type="l",xlab="")
            points(Sub_metering_2,type="l",col="red")
            points(Sub_metering_3,type="l",col="blue")
            legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
